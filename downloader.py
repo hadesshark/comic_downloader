4453da59e15f7c0f0d94c187ef48c2fb1e7de3e8
hadesshark <hades_shark@hotmail.com>
2017-04-28 23:01:29 +0800

    原來是忘記加上 wb

--- downloader.py
+++ downloader.py
@@ -19,4 +19,21 @@ page_con_encode = page.text.encode('utf-8')
 __xpath_for_images = u"//div[@class='thumb-container']/a[@class='gallerythumb']/img/@data-src"
 page_ready_to_xpath = etree.HTML(page_con_encode)
 temp_images_list = page_ready_to_xpath.xpath(__xpath_for_images)
-print(temp_images_list)
+
+# 有抓到圖片 url
+# print(temp_images_list)
+
+# 直接計算數量
+num_images = len(temp_images_list)
+# 抓取用網址重要數字
+url_num = temp_images_list[1].split('/')[-2]
+for item in range(num_images):
+    print(item + 1)
+
+    temp_url = r"https://i.nhentai.net/galleries/" + str(url_num) + '/'
+
+    # print(temp_url + str(item + 1) + '.jpg')
+    with open('./download/test/' + str(item + 1) + '.jpg', 'wb') as outf:
+        data = requests.get(temp_url + str(item + 1) + '.jpg')
+        if data.status_code == 200:
+            outf.write(data.content)

e48b84f1717418745db4b96052a1f4e4d206511c
hadesshark <hades_shark@hotmail.com>
2017-04-29 20:35:01 +0800

    雖然可以下載圖片，但真的太慢了

--- downloader.py
+++ downloader.py
@@ -3,15 +3,18 @@ import requests
 from lxml import etree
 import os
 
+from gzip import GzipFile
+from io import StringIO
+
 # 壓縮用目前先整理程式碼
 # from PIL import Image
 
-url = "https://nhentai.net/g/128181/"
+url = "https://nhentai.net/g/193536/"
 
 headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36',
-        'Accept-Encoding':
+        'Accept-encoding',
         'gzip'}
 
 page = requests.get(url, headers)
@@ -26,9 +29,10 @@ page_ready_to_xpath = etree.HTML(page_con_encode)
 temp_title = page_ready_to_xpath.xpath(__xpath_for_title)
 temp_images_list = page_ready_to_xpath.xpath(__xpath_for_images)
 
-print(temp_title)
-if not os.path.exists('./download/' + temp_title[0]):
-    os.makedirs('./download/' + temp_title[0])
+print(temp_title[0])
+file_save_string = './download/' + temp_title[0]
+if not os.path.exists(file_save_string):
+    os.makedirs(file_save_string)
 
 # 有抓到圖片 url
 # print(temp_images_list)
@@ -37,13 +41,23 @@ if not os.path.exists('./download/' + temp_title[0]):
 num_images = len(temp_images_list)
 # 抓取用網址重要數字
 url_num = temp_images_list[1].split('/')[-2]
+print(url_num)
+
+# 出現關於不同格式圖片的問題
 for item in range(num_images):
     print(item + 1)
 
+    # print(temp_images_list[item].split('.')[-1])
+    kind_of_image_file = '.' + temp_images_list[item].split('.')[-1]
+
     temp_url = r"https://i.nhentai.net/galleries/" + str(url_num) + '/'
 
+    file_name_or_download_name = str(item + 1) + kind_of_image_file
+
     # print(temp_url + str(item + 1) + '.jpg')
-    with open('./download/' + temp_title[0] + '/' + str(item + 1) + '.jpg', 'wb') as outf:
-        data = requests.get(temp_url + str(item + 1) + '.jpg')
+    with open(file_save_string + '/' + file_name_or_download_name, 'wb') as outf:
+        data = requests.get(temp_url + file_name_or_download_name)
+        print(data.status_code)
+        print(temp_url + file_name_or_download_name)
         if data.status_code == 200:
             outf.write(data.content)

d50f4d322d65a3030c506a70581cd28a56e6ceea
hadesshark <hades_shark@hotmail.com>
2017-04-29 08:54:19 +0800

    好像不能 .gitignore

--- downloader.py
+++ downloader.py
@@ -1,24 +1,34 @@
 # -*- coding: utf-8 -*-
 import requests
 from lxml import etree
-import json
-import sys
+import os
 
-url = "https://nhentai.net/g/182867/"
+# 壓縮用目前先整理程式碼
+# from PIL import Image
+
+url = "https://nhentai.net/g/120950/"
 
 headers = {
         'User-Agent':
-        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'}
+        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept-Encoding':
+        'gzip'}
 
 page = requests.get(url, headers)
 page_con_encode = page.text.encode('utf-8')
 
 # 確定可以抓取
 # print(page_con_encode)
-
+__xpath_for_title = u"//div[@id='info']/h2//text()"
 __xpath_for_images = u"//div[@class='thumb-container']/a[@class='gallerythumb']/img/@data-src"
 page_ready_to_xpath = etree.HTML(page_con_encode)
+
+temp_title = page_ready_to_xpath.xpath(__xpath_for_title)
 temp_images_list = page_ready_to_xpath.xpath(__xpath_for_images)
+
+print(temp_title)
+if not os.path.exists('./download/' + temp_title[0]):
+    os.makedirs('./download/' + temp_title[0])
 
 # 有抓到圖片 url
 # print(temp_images_list)
@@ -33,7 +43,7 @@ for item in range(num_images):
     temp_url = r"https://i.nhentai.net/galleries/" + str(url_num) + '/'
 
     # print(temp_url + str(item + 1) + '.jpg')
-    with open('./download/test/' + str(item + 1) + '.jpg', 'wb') as outf:
+    with open('./download/' + temp_title[0] + '/' + str(item + 1) + '.jpg', 'wb') as outf:
         data = requests.get(temp_url + str(item + 1) + '.jpg')
         if data.status_code == 200:
             outf.write(data.content)
